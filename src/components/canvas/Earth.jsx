9502e68ce2a786d0c52cd514badc7e223f036c43
Aman Raj <amanraj6221@gmail.com>
2026-03-27 21:32:38 +0530

    Updated

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,3 +1,4 @@
+// C:\Users\Aman Mehra\amanreactportfolio\amanreactportfolio\src\components\canvas\Earth.jsx
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
